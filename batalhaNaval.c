7afedc47138e84e82b4698fe5d207013d479491f
WiliamMaia <wiliam.maia.silva@gmail.com>
2025-03-20 13:02:19 +0000

    # Iniciei o desafio novato e passei a manhã toda pesquisando e estudando como exibir e montar um tabuleiro sem ter de criar uma matriz manual linha por linha, utilizei recursividades e loops e aprendi um conceito de definição no caminho ! Pensei em colocar um 'if-else' para garantir que em scanf não fosse aplicado o navio em posições que não caibam no tabuleiro mas como ainda não foi pedido interação ... melhor deixar simples por enquanto ...

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,9 +4,39 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
+# define tamanho 10 // Aqui defini uma variável para o tamanho do tabuleiro  com o valor '10', assim quando aplicar o tamanho tanto na horizontal quanto na vertical será um quadrado de tamanho 10
+
+void exibicao_tabuleiro (int tabuleiro [tamanho][tamanho]) {   //  Aqui declaro uma recursividade para exibir o tabuleiro
+    printf("   A B C D E F G H I J\n");  // Aqui quero exibir o cabeçalho do tabuleiro na horizontal para melhor coordenadas, precisei colocar 3 espaços antes do A para melhor alinjamento mas inda estou com problemas na linha 10
+    for (int x = 0; x < tamanho; x ++) {  //  Aqui declaro um loop para as colunas serem criadas
+        printf("%d:", x + 1);   // Aqui estou tentando enumerar cada linha com um numeral, como se começa com '0' pedi para que somasse + 1 a fim de mostrar de 1 - 10
+        for (int y = 0; y < tamanho; y ++) {  //  Aqui declaro outro loop para serem creiadas as colunas
+            printf(" %d", tabuleiro[x][y]);
+        }
+        printf("\n");
+    }
+}
+void navio_horizontal (int tabuleiro[tamanho][tamanho], int linha, int coluna) {  // Aqui declaro uma recursividade para dicionar um navio na horizontal, precisei declarar variáveis linha e coluna para apliccar as modificações
+    for (int x = 0; x < 3; x ++) {  //  Aqui declaro um loop para aplicar o navio que ocupa 3 posições na horizontal (x)
+        tabuleiro[linha][coluna + x] = 3;  //  Aqui será aplicado 0 '3' nas colunas e na linha
+    }
+}
+void navio_vertical (int tabuleiro[tamanho][tamanho], int linha, int coluna) {  //  Aqui declaro a recursividade para o navio na vertical
+    for (int y = 0; y < 3; y ++) {  //  Aqui declaro um loop para aplicar o navio na vertical (y) que ocupará também 3 posições
+        tabuleiro[linha + y][coluna] = 3;   //  Aqui será aplicado p '3' nas linhas e colunas
+    }
+}
+
 int main() {
+
+    int tabuleiro[tamanho][tamanho] = {0};   //  Aqui quero iniciar o tabuleiro com todos os valores '0' para serem representados por àgua
+
+    navio_horizontal(tabuleiro, 2, 2);  //  Aqui chamo a recursividade do navio na horizontal, coloquei como posição inicial 'C3' no tabuleiro e ocupou 'C3, D3 e E3'
+    navio_vertical(tabuleiro, 6, 6);    //  Aqui chamo a recursividade do navio na vertical, coloquei como posição inicial 'G7' no tabuleiro e ocupou 'G7, G8 e G9'
+    exibicao_tabuleiro(tabuleiro);   //  Aqui chamo a recursividade da exibição do tabuleiro
+
+    
     // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
